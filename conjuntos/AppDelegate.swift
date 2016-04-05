d2a43e2f0a8b1399122a6fc3257472e7eb439fc8
mariodiar <mariodiar@gmail.com>
2016-04-05 01:20:50 -0500

    Fixes color of main views

--- conjuntos/AppDelegate.swift
+++ conjuntos/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = UIColor(red: 55, green: 55, blue:55, alpha: 1.0)
+        
         return true
     }
 
