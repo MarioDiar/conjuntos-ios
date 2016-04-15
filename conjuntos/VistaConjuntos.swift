6d7dcd8f6f571084cc6ef649d37fbf871685f2be
mariodiar <mariodiar@gmail.com>
2016-04-15 02:08:00 -0500

    Draws names of conjuntos

--- conjuntos/VistaConjuntos.swift
+++ conjuntos/VistaConjuntos.swift
@@ -5,32 +5,6 @@
 //  Created by Mario Díaz on 3/15/16.
 //  Copyright © 2016 Mario Díaz. All rights reserved.
 
-//TODO: Fix the view of 2 conjuntos 
-
-//var s: NSString=NSString(string: "SUGUS");
-//var iTextPositionX:Int = 100;
-//var iTextPositionY:Int = 100;
-//
-//let fieldColor: UIColor = UIColor.darkGrayColor()
-//let fieldFont = UIFont(name: "Helvetica Neue", size: 12)
-//var paraStyle = NSMutableParagraphStyle()
-//
-//paraStyle.lineSpacing = 6.0
-//paraStyle.alignment = NSTextAlignment.Center;// <---
-//var skew = 0.1
-//
-//let attributes = [
-//    NSForegroundColorAttributeName: fieldColor,
-//    NSParagraphStyleAttributeName: paraStyle,
-//    NSObliquenessAttributeName: skew,
-//    //NSTextAlignment: textalign,
-//    NSFontAttributeName: fieldFont!
-//]
-//
-//var Textpoint:CGPoint=CGPoint(x: iTextPositionX, y: iTextPositionY);
-//s.drawAtPoint(Textpoint, withAttributes: attributes);
-
-
 import UIKit
 
 class VistaConjuntos: UIView {
@@ -44,10 +18,14 @@ class VistaConjuntos: UIView {
         //Contexto donde se dibujara
         let contexto = UIGraphicsGetCurrentContext()
         //Colores transparentes
-        let negroTrans = CGColorCreateCopyWithAlpha(UIColor.orangeColor().CGColor, 0.5)
+        let orangeTrans = CGColorCreateCopyWithAlpha(UIColor.orangeColor().CGColor, 0.5)
         let magentaTrans = CGColorCreateCopyWithAlpha(UIColor.magentaColor().CGColor, 0.5)
-        let blancoTrans = CGColorCreateCopyWithAlpha(UIColor.cyanColor().CGColor, 0.5)
-        let transparent = CGColorCreateCopyWithAlpha(UIColor.blackColor().CGColor, 0.0)
+        let cyanTrans = CGColorCreateCopyWithAlpha(UIColor.cyanColor().CGColor, 0.5)
+        let transparent = CGColorCreateCopyWithAlpha(UIColor.clearColor().CGColor, 0.0)
+        
+        let nameConj1 = "A"
+        let nameConj2 = "B"
+        let nameConj3 = "C"
         
         //setting font attributes
         let font = UIFont(name: "Helvetica Bold", size: 14.0)
@@ -65,19 +43,18 @@ class VistaConjuntos: UIView {
         CGContextSetLineWidth(contexto, 1.0)
         CGContextSetStrokeColorWithColor(contexto, UIColor.blackColor().CGColor)
 
-        
         if numConjuntos == 2 {
             //DRAWING ELLIPSES
             //Elipse-setting elipse stroke and fill color
             
-            CGContextSetFillColorWithColor(contexto, negroTrans)
+            CGContextSetFillColorWithColor(contexto, cyanTrans)
             
             let theRect: CGRect = CGRectMake(100, 50, 150, 150)
             CGContextFillEllipseInRect(contexto, theRect)
             CGContextStrokeEllipseInRect(contexto, theRect)
             
             //Setting new colors to stroke and fill
-            CGContextSetFillColorWithColor(contexto, magentaTrans)
+            CGContextSetFillColorWithColor(contexto, orangeTrans)
             
             //Elipse
             let elipse: CGRect = CGRectMake(25, 50, 150, 150)
@@ -91,9 +68,15 @@ class VistaConjuntos: UIView {
             let botRightConj = CGRectMake(177, 122, 60, 50)
             CGContextFillRect(contexto, botRightConj)
             
+            let nameBotRightConj = CGRectMake(220, 49, 15, 15)
+            CGContextFillRect(contexto, nameBotRightConj)
+            
             //botleft rectangulo
             let botLeftCon = CGRectMake(38, 120, 60, 50)
             CGContextFillRect(contexto, botLeftCon)
+            
+            let nameBotLeftConj = CGRectMake(40, 49, 15, 15)
+            CGContextFillRect(contexto, nameBotLeftConj)
             
             //intersection
             let inter = CGRectMake(117, 116, 44, 44)
@@ -102,13 +85,16 @@ class VistaConjuntos: UIView {
             (diagrama.arrayToString(diagrama.conj1) as NSString).drawInRect(botLeftCon, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.conj2) as NSString).drawInRect(botRightConj, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.inters12) as NSString).drawInRect(inter, withAttributes: textFontAttributes)
+            
+            (nameConj1 as NSString).drawInRect(nameBotLeftConj, withAttributes: textFontAttributes)
+            (nameConj2 as NSString).drawInRect(nameBotRightConj, withAttributes: textFontAttributes)
         
             
             
         } else if numConjuntos == 3 {
             //DRAWING ELLIPSES
             //Elipse-setting elipse stroke and fill color
-            CGContextSetFillColorWithColor(contexto, negroTrans)
+            CGContextSetFillColorWithColor(contexto, orangeTrans)
             
             let theRect: CGRect = CGRectMake(65, 15, 150, 150)
             CGContextFillEllipseInRect(contexto, theRect)
@@ -123,7 +109,7 @@ class VistaConjuntos: UIView {
             CGContextStrokeEllipseInRect(contexto, elipse)
             
             //Setting new colors to stroke and fill
-            CGContextSetFillColorWithColor(contexto, blancoTrans)
+            CGContextSetFillColorWithColor(contexto, cyanTrans)
             
             //Elipse
             let elipseTwo: CGRect = CGRectMake(100, 85, 150, 150)
@@ -138,13 +124,22 @@ class VistaConjuntos: UIView {
             let topConj: CGRect = CGRectMake(115, 30, 60, 50)
             CGContextFillRect(contexto, topConj)
             
+            let nameTopConj = CGRectMake(95, 5, 15, 15)
+            CGContextFillRect(contexto, nameTopConj)
+            
             //botright rectangulo
             let botRightConj = CGRectMake(177, 157, 60, 50)
             CGContextFillRect(contexto, botRightConj)
             
+            let nameBotRightConj = CGRectMake(220, 219, 15, 15)
+            CGContextFillRect(contexto, nameBotRightConj)
+            
             //botleft rectangulo
-            let botLeftCon = CGRectMake(38, 155, 60, 50)
-            CGContextFillRect(contexto, botLeftCon)
+            let botLeftConj = CGRectMake(38, 155, 60, 50)
+            CGContextFillRect(contexto, botLeftConj)
+            
+            let nameBotLeftConj = CGRectMake(40, 219, 15, 15)
+            CGContextFillRect(contexto, nameBotLeftConj)
             
             //inter right
             let interRight = CGRectMake(165, 90, 40, 40)
@@ -162,14 +157,20 @@ class VistaConjuntos: UIView {
             let interTri = CGRectMake(117, 116, 44, 44)
             CGContextFillRect(contexto, interTri)
             
-            //DRAWING TEXT ON RECTANGLES
+            
+            //DRAWING DATA ON RECTANGLES
             (diagrama.arrayToString(diagrama.conj1) as NSString).drawInRect(topConj, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.conj2) as NSString).drawInRect(botRightConj, withAttributes: textFontAttributes)
-            (diagrama.arrayToString(diagrama.conj3) as NSString).drawInRect(botLeftCon, withAttributes: textFontAttributes)
+            (diagrama.arrayToString(diagrama.conj3) as NSString).drawInRect(botLeftConj, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.inters12) as NSString).drawInRect(interRight, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.inters13) as NSString).drawInRect(interLeft, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.inters23) as NSString).drawInRect(interBottom, withAttributes: textFontAttributes)
             (diagrama.arrayToString(diagrama.inters123) as NSString).drawInRect(interTri, withAttributes: textFontAttributes)
+            
+            //DRAWING NAMES ON CONJUNTOS
+            (nameConj1 as NSString).drawInRect(nameTopConj, withAttributes: textFontAttributes)
+            (nameConj2 as NSString).drawInRect(nameBotRightConj, withAttributes: textFontAttributes)
+            (nameConj3 as NSString).drawInRect(nameBotLeftConj, withAttributes: textFontAttributes)
 
             //DEBUGGING
 //            print("DATOS CONJ 1: ")
